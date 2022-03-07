16345daab7b0656fe12bbdec2254f2fbdab70bfc
Andrey Radchishin <andrey@andreyrd.com>
2022-03-07 17:38:10 -0500

    Update vue config to handle max-len better

--- vue.js
+++ vue.js
@@ -5,7 +5,17 @@ module.exports = {
     ],
     rules: {
         'vue/html-indent': [ 'error', 4 ],
+        'vue/max-len': [ 'error', { code: 120, ignoreHTMLTextContents: true } ],
         'vue/multi-word-component-names': 'off',
         'vue/no-unused-components': 'warn',
     },
+    overrides: [
+        {
+            files: [ '*.vue' ],
+            rules: {
+                'max-len': 'off',
+            },
+        },
+    ],
 };
+
