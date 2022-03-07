455b284e2c3a7443b319ffa463f5bd228c2e41fa
Andrey Radchishin <andrey@andreyrd.com>
2022-03-07 17:10:04 -0500

    Potentially fix stylelint

--- stylelint.js
+++ stylelint.js
@@ -1,5 +1,5 @@
 module.exports = {
-    extends: 'stylelint-config-standard-scss',
+    extends: [ 'stylelint-config-standard-scss', 'stylelint-config-standard-vue/scss' ],
     ignoreFiles: [
       './dist/**/*',
       './node_modules/**/*',
